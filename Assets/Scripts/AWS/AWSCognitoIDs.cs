8e0ad03914d933421e04f3fa498fffadd85a2c1a
pvicugna11 <pvicugna11@gmail.com>
2022-12-19 02:57:52 +0900

    フォルダ整理

--- Assets/Scripts/AWS/AWSCognitoIDs.cs
+++
@@ -1,5 +0,0 @@
-public class AWSCognitoIDs
-{
-    public const string UserPoolId = "ap-northeast-1_0orX6Vlqz";
-    public const string AppClientId = "5cmcg1tththambk9endirv44me";
-}
---
+++ Assets/Scripts/Common/AWS/AWSCognitoIDs.cs
@@ -0,0 +1,5 @@
+public class AWSCognitoIDs
+{
+    public const string UserPoolId = "ap-northeast-1_kDKDSSg8w";
+    public const string AppClientId = "30ug7mri6q841972nrc9koij2f";
+}
